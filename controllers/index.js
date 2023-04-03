54795b058cc05bee5d7b20c5f29911b06cdedc4d
David Mackey <davidmackey92@gmail.com>
2023-04-03 08:16:25 -0500

    index - router, apiRoutes, homeRoutes, export

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,17 @@
-import { Router } from 'express';
-import apiRoutes from './api';
-import homeRoutes from './homeRoutes';
+// This file is the main controller file that will be used to route all of the requests to the correct controller file
+const router = require('express').Router();
 
-const router = Router();
+// Import all of the API routes from /api/index.js
+const apiRoutes = require('./api');
 
+// Import all of the home page routes from /homeRoutes.js
+const homeRoutes = require('./homeRoutes');
+
+// Add prefix of `/` to all of the home page routes imported from the `homeRoutes` directory
 router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
 
-export default router;
+// Add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use('/api', apiRoutes); // This is the route that will be used to access the API routes
+
+// Export the router
+module.exports = router;
