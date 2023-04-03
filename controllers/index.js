399ac6c429222e31f3e1b38b91e2c0fa886f6b9b
David Mackey <davidmackey92@gmail.com>
2023-04-03 07:31:14 -0500

    index controller, es6 syntax

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import apiRoutes from './api';
+import homeRoutes from './homeRoutes';
+
+const router = Router();
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+export default router;
