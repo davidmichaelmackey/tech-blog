53acea375e170a1d073140fd708d84fd8bd48b99
David Mackey <davidmackey92@gmail.com>
2023-04-04 19:50:24 -0500

    debugging last few errors

--- controllers/api/createpost-routes.js
+++ controllers/api/createpost-routes.js
@@ -6,12 +6,26 @@ const router = require('express').Router();
 const { User, Post } = require('../../models');
 
 // withAuth
-// const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 
 // create a new post
-router.post('/', async (req, res) => {
-  const userData = await Post.create(req.body);
-  res.json(userData);
+// router.post('/', async (req, res) => {
+//   const userData = await Post.create(req.body);
+//   res.json(userData);
+// });
+
+router.post("/", withAuth, (req, res) => {
+  console.log("creating");
+  Post.create({
+    title: req.body.title,
+    content: req.body.post_content,
+    user_id: req.session.user_id
+  })
+    .then((userData) => res.json(userData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // export the router
