c385f3efe0150f2f6a51d8d8f21466820a8b4983
David Mackey <davidmackey92@gmail.com>
2023-04-04 20:22:19 -0500

    fixed path to create post

--- controllers/api/createpost-routes.js
+++ controllers/api/createpost-routes.js
@@ -9,23 +9,9 @@ const { User, Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // create a new post
-// router.post('/', async (req, res) => {
-//   const userData = await Post.create(req.body);
-//   res.json(userData);
-// });
-
-router.post("/", withAuth, (req, res) => {
-  console.log("creating");
-  Post.create({
-    title: req.body.title,
-    content: req.body.post_content,
-    user_id: req.session.user_id
-  })
-    .then((userData) => res.json(userData))
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+router.post('/', async (req, res) => {
+  const userData = await Post.create(req.body);
+  res.json(userData);
 });
 
 // export the router
